351b27e084e1ff573127b0151f6792bfca818dbf
Faq <69550808+Faq-hue@users.noreply.github.com>
2022-06-10 17:36:15 -0300

    Add files via upload

---
+++ back-end/App.java
@@ -0,0 +1,12 @@
+import server.Server;
+
+/**
+ * app
+ */
+public class App {
+
+    public static void main(String[] args) {
+        Server s1=new Server();
+        s1.IniciarServidor(8866);
+    }
+}
